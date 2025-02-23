42cdca06709068d3d4ff9dbb576436166a18f7eb
Steve.Krushelniski <steve.krushelniski.external@autoliv.com>
2025-02-23 10:05:05 -0600

    latest push

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -124,7 +124,7 @@ def step_impl(context, name):
     element = context.driver.find_element(By.ID, 'search_results')
     assert(name not in element.text)
 
-@then('I should see message "{message}"')
+@then('I should see the message "{message}"')
 def step_impl(context, message):
     found = WebDriverWait(context.driver, context.wait_seconds).until(
         expected_conditions.text_to_be_present_in_element(
